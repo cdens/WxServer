c3ad1735f1608580f2a29430101f614316b2922d
Casey Densmore <cdensmore101@gmail.com>
2021-08-01 17:19:28 -0500

    Overview info, readme update

--- app.py
+++ app.py
@@ -397,87 +397,93 @@ def plot_styler(p):
     
 def observations_plot(obstoplot):
     
-    #parsing out observations in range to be plotted
-    date,temp,rh,pres,wspd,wdir,precip,solar,strikes = parsedboutput(obstoplot)
-    source = ColumnDataSource(data={"date":date, "temp":temp, "rh":rh, "pres":pres, "wspd":wspd, "wdir":wdir, "precip":precip, "solar":solar, "strikes":strikes}) #organizing data into columndatasource format
+    try:
+        #parsing out observations in range to be plotted
+        date,temp,rh,pres,wspd,wdir,precip,solar,strikes = parsedboutput(obstoplot)
+        source = ColumnDataSource(data={"date":date, "temp":temp, "rh":rh, "pres":pres, "wspd":wspd, "wdir":wdir, "precip":precip, "solar":solar, "strikes":strikes}) #organizing data into columndatasource format
+        
+        
+        #initializing figure
+        p = figure(height=400, width=1500, title='', x_axis_type="datetime", toolbar_location="above",
+            tools="pan,wheel_zoom,box_zoom,reset")
+        p.extra_y_ranges = {}
+        p.yaxis.visible = False #drop default y axis
+        
+        #temperature
+        p.extra_y_ranges["temp"] = Range1d(start=np.floor(np.min(temp))-1, end=np.ceil(np.max(temp))+1)
+        p.add_layout(LinearAxis(y_range_name="temp", axis_line_color="red"), 'left')
+        p.line(x="date", y="temp", source=source, line_color="red", name="Temperature", y_range_name="temp", legend_label="Temperature")
+        p.circle(x="date", y="temp", source=source, color="red", name="Temperature", y_range_name="temp",  legend_label="Temperature")
+        
+        #humidity
+        p.extra_y_ranges["rh"] = Range1d(start=np.floor(np.min(rh))-1, end=np.ceil(np.max(rh))+1)
+        p.add_layout(LinearAxis(y_range_name="rh", axis_line_color="blue"), 'left')
+        p.line(x="date", y="rh", source=source, line_color="blue", name="Humidity", y_range_name="rh", legend_label="Humidity")
+        p.circle(x="date", y="rh", source=source, color="blue", name="Humidity", y_range_name="rh", legend_label="Humidity")
+        
+        #pressure
+        p.extra_y_ranges["pres"] = Range1d(start=np.floor(np.min(pres))-1, end=np.ceil(np.max(pres))+1)
+        p.add_layout(LinearAxis(y_range_name="pres", axis_line_color="green"), 'left')
+        p.line(x="date", y="pres", source=source, line_color="green", name="Pressure", y_range_name="pres", legend_label="Pressure")
+        p.circle(x="date", y="pres", source=source, color="green", name="Pressure", y_range_name="pres", legend_label="Pressure")
+        
+        #wind speed
+        p.extra_y_ranges["wspd"] = Range1d(start=np.floor(np.min(wspd))-1, end=np.ceil(np.max(wspd))+1)
+        p.add_layout(LinearAxis(y_range_name="wspd", axis_line_color="orange"), 'left')
+        p.line(x="date", y="wspd", source=source, line_color="orange", name="Wind Speed", y_range_name="wspd", legend_label="Wind Speed")
+        p.circle(x="date", y="wspd", source=source, color="orange", name="Wind Speed", y_range_name="wspd", legend_label="Wind Speed")
+        
+        #wind direction
+        p.extra_y_ranges["wdir"] = Range1d(start=np.floor(np.min(wdir))-1, end=np.ceil(np.max(wdir))+1)
+        p.add_layout(LinearAxis(y_range_name="wdir", axis_line_color="purple"), 'left')
+        p.circle(x="date", y="wdir", source=source, color="purple", name="Wind Direction", y_range_name="wdir", legend_label="Wind Direction")
+        
+        #precipitation TODO- MAKE BAR
+        p.extra_y_ranges["precip"] = Range1d(start=np.floor(np.min(precip)), end=np.ceil(np.max(precip))+1)
+        p.add_layout(LinearAxis(y_range_name="precip", axis_line_color="blue"), 'left')
+        p.vbar(x="date",top="precip", width = .9, fill_alpha = .5, fill_color = 'blue', line_alpha = .5, line_color='blue', source=source, name="Precipitation", y_range_name="precip", legend_label="Precipitation")
     
+        
+        #solar radiation
+        p.extra_y_ranges["solar"] = Range1d(start=np.floor(np.min(solar))-1, end=np.ceil(np.max(solar))+1)
+        p.add_layout(LinearAxis(y_range_name="solar", axis_line_color="yellow"), 'left')
+        p.line(x="date", y="solar", source=source, line_color="yellow", name="Solar Radiation", y_range_name="solar", legend_label="Solar Radiation")
+        p.circle(x="date", y="solar", source=source, color="yellow", name="Solar Radiation", y_range_name="solar", legend_label="Solar Radiation")
+        
+        #lightning strikes
+        p.extra_y_ranges["strikes"] = Range1d(start=np.floor(np.min(strikes)), end=np.ceil(np.max(strikes))+1)
+        p.add_layout(LinearAxis(y_range_name="strikes", axis_line_color="yellow"), 'left')
+        p.vbar(x="date",top="strikes", width = .9, fill_alpha = .5, fill_color = 'yellow', line_alpha = .5, line_color='yellow', source=source, name="Lightning Strikes", y_range_name="strikes", legend_label="Lightning Strikes")
+        
+        
+        #adding legend
+        p.legend.location = "top_left"
+        p.legend.click_policy="hide"
+        
+        
+        #adding hover tool
+        TOOLTIPS=[("Date", "@date{%Y-%m-%d %H:%M}"), 
+                    ("Temperature (C)", "@temp{00.0}"), 
+                    ("Humidity (%)", "@rh{00.0}"), 
+                    ("Pressure (mb)", "@pres{0000.0}"),
+                    ("Wind Speed (mph)", "@wspd{0.0}"),
+                    ("Wind Direction", "@wdir{000}"),
+                    ("Precipitation (mm/hr)", "@precip{0.0}"),
+                    ("Solar Radiation", "@solar"),
+                    ("Lightning Strikes (per hour)", "@strikes{0.0}")] #setting tooltips for interactive hover
+        hovertool = HoverTool(tooltips=TOOLTIPS, formatters={'@date': 'datetime'}) # use 'datetime' formatter for '@date' field
+        p.add_tools(hovertool)
+        
+        plot_styler(p) #applying global stylings for plot
+        
+        script,div = components(p) #pulling javascript/html components to embed in webpage
+        
+        return script + div
     
-    #initializing figure
-    p = figure(height=400, width=1500, title='', x_axis_type="datetime", toolbar_location="above",
-        tools="pan,wheel_zoom,box_zoom,reset")
-    p.extra_y_ranges = {}
-    p.yaxis.visible = False #drop default y axis
-    
-    #temperature
-    p.extra_y_ranges["temp"] = Range1d(start=np.floor(np.min(temp))-1, end=np.ceil(np.max(temp))+1)
-    p.add_layout(LinearAxis(y_range_name="temp", axis_line_color="red"), 'left')
-    p.line(x="date", y="temp", source=source, line_color="red", name="Temperature", y_range_name="temp", legend_label="Temperature")
-    p.circle(x="date", y="temp", source=source, color="red", name="Temperature", y_range_name="temp",  legend_label="Temperature")
-    
-    #humidity
-    p.extra_y_ranges["rh"] = Range1d(start=np.floor(np.min(rh))-1, end=np.ceil(np.max(rh))+1)
-    p.add_layout(LinearAxis(y_range_name="rh", axis_line_color="blue"), 'left')
-    p.line(x="date", y="rh", source=source, line_color="blue", name="Humidity", y_range_name="rh", legend_label="Humidity")
-    p.circle(x="date", y="rh", source=source, color="blue", name="Humidity", y_range_name="rh", legend_label="Humidity")
-    
-    #pressure
-    p.extra_y_ranges["pres"] = Range1d(start=np.floor(np.min(pres))-1, end=np.ceil(np.max(pres))+1)
-    p.add_layout(LinearAxis(y_range_name="pres", axis_line_color="green"), 'left')
-    p.line(x="date", y="pres", source=source, line_color="green", name="Pressure", y_range_name="pres", legend_label="Pressure")
-    p.circle(x="date", y="pres", source=source, color="green", name="Pressure", y_range_name="pres", legend_label="Pressure")
-    
-    #wind speed
-    p.extra_y_ranges["wspd"] = Range1d(start=np.floor(np.min(wspd))-1, end=np.ceil(np.max(wspd))+1)
-    p.add_layout(LinearAxis(y_range_name="wspd", axis_line_color="orange"), 'left')
-    p.line(x="date", y="wspd", source=source, line_color="orange", name="Wind Speed", y_range_name="wspd", legend_label="Wind Speed")
-    p.circle(x="date", y="wspd", source=source, color="orange", name="Wind Speed", y_range_name="wspd", legend_label="Wind Speed")
-    
-    #wind direction
-    p.extra_y_ranges["wdir"] = Range1d(start=np.floor(np.min(wdir))-1, end=np.ceil(np.max(wdir))+1)
-    p.add_layout(LinearAxis(y_range_name="wdir", axis_line_color="purple"), 'left')
-    p.circle(x="date", y="wdir", source=source, color="purple", name="Wind Direction", y_range_name="wdir", legend_label="Wind Direction")
-    
-    #precipitation TODO- MAKE BAR
-    p.extra_y_ranges["precip"] = Range1d(start=np.floor(np.min(precip)), end=np.ceil(np.max(precip))+1)
-    p.add_layout(LinearAxis(y_range_name="precip", axis_line_color="blue"), 'left')
-    p.vbar(x="date",top="precip", width = .9, fill_alpha = .5, fill_color = 'blue', line_alpha = .5, line_color='blue', source=source, name="Precipitation", y_range_name="precip", legend_label="Precipitation")
-
-    
-    #solar radiation
-    p.extra_y_ranges["solar"] = Range1d(start=np.floor(np.min(solar))-1, end=np.ceil(np.max(solar))+1)
-    p.add_layout(LinearAxis(y_range_name="solar", axis_line_color="yellow"), 'left')
-    p.line(x="date", y="solar", source=source, line_color="yellow", name="Solar Radiation", y_range_name="solar", legend_label="Solar Radiation")
-    p.circle(x="date", y="solar", source=source, color="yellow", name="Solar Radiation", y_range_name="solar", legend_label="Solar Radiation")
-    
-    #lightning strikes
-    p.extra_y_ranges["strikes"] = Range1d(start=np.floor(np.min(strikes)), end=np.ceil(np.max(strikes))+1)
-    p.add_layout(LinearAxis(y_range_name="strikes", axis_line_color="yellow"), 'left')
-    p.vbar(x="date",top="strikes", width = .9, fill_alpha = .5, fill_color = 'yellow', line_alpha = .5, line_color='yellow', source=source, name="Lightning Strikes", y_range_name="strikes", legend_label="Lightning Strikes")
-    
-    
-    #adding legend
-    p.legend.location = "top_left"
-    p.legend.click_policy="hide"
-    
-    
-    #adding hover tool
-    TOOLTIPS=[("Date", "@date{%Y-%m-%d %H:%M}"), 
-                ("Temperature (C)", "@temp{00.0}"), 
-                ("Humidity (%)", "@rh{00.0}"), 
-                ("Pressure (mb)", "@pres{0000.0}"),
-                ("Wind Speed (mph)", "@wspd{0.0}"),
-                ("Wind Direction", "@wdir{000}"),
-                ("Precipitation (mm/hr)", "@precip{0.0}"),
-                ("Solar Radiation", "@solar"),
-                ("Lightning Strikes (per hour)", "@strikes{0.0}")] #setting tooltips for interactive hover
-    hovertool = HoverTool(tooltips=TOOLTIPS, formatters={'@date': 'datetime'}) # use 'datetime' formatter for '@date' field
-    p.add_tools(hovertool)
-    
-    plot_styler(p) #applying global stylings for plot
-    
-    script,div = components(p) #pulling javascript/html components to embed in webpage
-    
-    return script + div
+    except ValueError:
+        
+        return "No data available within the specified time period!"
+        
     
 
     
